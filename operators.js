68270f543ba2d73b15a4352ad5640414fb9d4816
panky-creator <pankajbinnar886@gmail.com>
2025-08-07 19:03:46 +0530

    updated code

--- operators.js
+++ operators.js
@@ -1,3 +1,32 @@
+//Assignment Operators Example
 let a=5;
 a+=3;
 console.log(a);
+
+a-=2;
+console.log(a);
+
+a*=2;
+console.log(a);
+
+a/=4;
+console.log(a);
+
+a%=3;
+console.log(a); 
+
+//comparison Operators Example
+ let str1= 5;
+let str2= "5";
+console.log(str1 == str2); // true, because values are equal    
+console.log(str1 === str2); // false, because types are different
+console.log(str1 != str2); // false, because values are equal
+console.log(str1 !== str2); // true, because types are different
+// Logical Operators Example
+let x = 5;
+let y = 10; 
+console.log(x < y && x > 0); // true, both conditions are true
+
+
+
+
